dd940518edbb84ed88a27f7d6a618d9c258b1493
SalmaSABAR477 <salmasabar777@gmail.com>
2026-06-20 15:19:04 +0100

    Refactor MedicalTreatmentApp start method
    Removed database initialization and error handling.

--- src/MedicalTreatmentApp.java
+++ src/MedicalTreatmentApp.java
@@ -1,37 +1,19 @@
-import controller.MainController;
-import dao.Database;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.MainView;
 
-import java.net.URL;
-
 public class MedicalTreatmentApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        try {
-            Database.initializeDatabase();
+        MainView mainView = new MainView();
 
-            MainView mainView = new MainView();
-            new MainController(mainView);
+        Scene scene = new Scene(mainView.getLayout(), 1200, 750);
 
-            Scene scene = new Scene(mainView.getLayout(), 1200, 750);
-
-            URL cssUrl = getClass().getResource("/resources/style.css");
-            if (cssUrl != null) {
-                scene.getStylesheets().add(cssUrl.toExternalForm());
-            }
-
-            primaryStage.setTitle("Suivi de Traitements Médicaux");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Erreur au lancement de l'application : " + e.getMessage());
-        }
+        primaryStage.setTitle("Suivi de Traitements Médicaux");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
