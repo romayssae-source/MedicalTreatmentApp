8743452d9b33b076f959b14e01e4f52b97616bdb
SalmaSABAR477 <salmasabar777@gmail.com>
2026-06-20 15:10:11 +0100

    Remove update and delete methods from PatientDAO
    Removed update and delete methods for patients, and refactored searchPatients method.

--- src/dao/PatientDAO.java
+++ src/dao/PatientDAO.java
@@ -36,49 +36,9 @@ public class PatientDAO {
         }
     }
 
-    public void updatePatient(Patient patient) throws SQLException {
-        String sql = """
-                UPDATE patients 
-                SET nom = ?, prenom = ?, date_naissance = ?, sexe = ?, telephone = ?, 
-                    remarques = ?, surveillance_active = ?
-                WHERE id = ?
-                """;
-
-        try (Connection connection = Database.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql)) {
-
-            ps.setString(1, patient.getNom());
-            ps.setString(2, patient.getPrenom());
-
-            if (patient.getDateNaissance() != null) {
-                ps.setDate(3, Date.valueOf(patient.getDateNaissance()));
-            } else {
-                ps.setNull(3, Types.DATE);
-            }
-
-            ps.setString(4, patient.getSexe());
-            ps.setString(5, patient.getTelephone());
-            ps.setString(6, patient.getRemarques());
-            ps.setBoolean(7, patient.isSurveillanceActive());
-            ps.setInt(8, patient.getId());
-
-            ps.executeUpdate();
-        }
-    }
-
-    public void deletePatient(int id) throws SQLException {
-        String sql = "DELETE FROM patients WHERE id = ?";
-
-        try (Connection connection = Database.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql)) {
-
-            ps.setInt(1, id);
-            ps.executeUpdate();
-        }
-    }
-
     public List<Patient> getAllPatients() throws SQLException {
         List<Patient> patients = new ArrayList<>();
+
         String sql = "SELECT * FROM patients ORDER BY id DESC";
 
         try (Connection connection = Database.getConnection();
@@ -93,57 +53,14 @@ public class PatientDAO {
         return patients;
     }
 
-    public List<Patient> searchPatients(String keyword, String sexe, Boolean surveillanceOnly) throws SQLException {
-        List<Patient> patients = new ArrayList<>();
-
-        StringBuilder sql = new StringBuilder("""
-                SELECT * FROM patients 
-                WHERE (nom LIKE ? OR prenom LIKE ? OR telephone LIKE ?)
-                """);
-
-        if (sexe != null && !sexe.equals("Tous")) {
-            sql.append(" AND sexe = ? ");
-        }
-
-        if (surveillanceOnly != null && surveillanceOnly) {
-            sql.append(" AND surveillance_active = TRUE ");
-        }
-
-        sql.append(" ORDER BY id DESC ");
-
-        try (Connection connection = Database.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql.toString())) {
-
-            String pattern = "%" + keyword + "%";
-
-            ps.setString(1, pattern);
-            ps.setString(2, pattern);
-            ps.setString(3, pattern);
-
-            int index = 4;
-
-            if (sexe != null && !sexe.equals("Tous")) {
-                ps.setString(index++, sexe);
-            }
-
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    patients.add(mapPatient(rs));
-                }
-            }
-        }
-
-        return patients;
-    }
-
     private Patient mapPatient(ResultSet rs) throws SQLException {
-        Date birthDate = rs.getDate("date_naissance");
+        Date date = rs.getDate("date_naissance");
 
         return new Patient(
                 rs.getInt("id"),
                 rs.getString("nom"),
                 rs.getString("prenom"),
-                birthDate != null ? birthDate.toLocalDate() : null,
+                date != null ? date.toLocalDate() : null,
                 rs.getString("sexe"),
                 rs.getString("telephone"),
                 rs.getString("remarques"),
